7018855de29759f1118bd837171f0f9cdcf08bcd
Schuyler Martin <schuylermartin45@gmail.com>
2023-11-27 09:26:08 -0700

    Type improvment learnings from `percy`
    - These came out of some learnings in another one of our projects

--- anaconda_packaging_utils/types.py
+++ anaconda_packaging_utils/types.py
@@ -2,14 +2,33 @@
 File:           types.py
 Description:    File that contains common types and type aliases (typedefs).
 """
-from typing import Any, Final, Mapping
+from typing import Final, Union
 
-# Recursive type for handling JSON data. See this post for more info:
-#   https://github.com/python/typing/issues/182#issuecomment-1320974824
-JsonType = dict[str, "JsonType"] | list["JsonType"] | str | int | float | bool | None
+# Base types that can store value
+Primitives = Union[str, int, float, bool, None]
+# Same primitives, as a tuple. Used with `isinstance()`
+PRIMITIVES_TUPLE: Final[tuple[type[str], type[int], type[float], type[bool], type[None]]] = (
+    str,
+    int,
+    float,
+    bool,
+    type(None),
+)
 
+# Type that represents a JSON-like type
+JsonType = Union[dict[str, "JsonType"], list["JsonType"], Primitives]
+
+# Types that build up to types used in `jsonschema`s
+SchemaPrimitives = Union[str, int, bool, None]
+SchemaDetails = Union[dict[str, "SchemaDetails"], list["SchemaDetails"], SchemaPrimitives]
 # Type for a schema object used by the `jsonschema` library
-SchemaType = Mapping[str, Any]  # type: ignore[misc]
+SchemaType = dict[str, SchemaDetails]
+
+
+# All sentinel values used in this module should be constructed with this class, for typing purposes.
+class SentinelType:
+    pass
+
 
 # Common exit codes
 EXIT_SUCCESS: Final[int] = 0
