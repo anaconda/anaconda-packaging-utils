c983b3c0626ffe004a065665c5a8fd10200c72b0
Schuyler Martin <schuylermartin45@gmail.com>
2023-11-28 16:26:15 -0700

    Finishes first-pass of `jsonschema` for new API
    - Also fixes static analyzer and linter issues

--- anaconda_packaging_utils/api/repodata_api.py
+++ anaconda_packaging_utils/api/repodata_api.py
@@ -6,18 +6,23 @@ Description:    Library that provides tooling for pulling and parsing `repodata.
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import Final, Optional, no_type_check
-
-from jsonschema import validate as schema_validate
+from typing import Final, Optional
 
 from anaconda_packaging_utils.api.types import BaseApiException
 from anaconda_packaging_utils.types import SchemaType
+
+# from jsonschema import validate as schema_validate
+
 
 # https://repo.anaconda.com/pkgs/mfain/noarch/repodata.json
 # channel / <architecture>
 
 
 class Channel(str, Enum):
+    """
+    Enumeration of channels supported on `repo.anaconda.com`
+    """
+
     MAIN = "main"
     FREE = "free"
     R = "r"
@@ -28,16 +33,20 @@ class Channel(str, Enum):
 
 
 class Architecture(str, Enum):
-    LINUX_x86_64 = "linux-64"
-    LINUX_x86_32 = "linux-32"
+    """
+    Enumeration of architectures supported on `repo.anaconda.com`
+    """
+
+    LINUX_X86_64 = "linux-64"
+    LINUX_X86_32 = "linux-32"
     LINUX_GRAVITON_2 = "linux-aarch64"
-    LINUX_ARM_v6l = "linux-armv6l"
-    LINUX_ARM_v7l = "linux-armv7l"
+    LINUX_ARM_V6L = "linux-armv6l"
+    LINUX_ARM_V7L = "linux-armv7l"
     LINUX_S390 = "linux-s390x"
     LINUX_PPC64LE = "linux-ppc64le"
     OSX_ARM64 = "osx-arm64"
-    OSX_x86_64 = "osx-64"
-    OSX_x86_32 = "osx-32"
+    OSX_X86_64 = "osx-64"
+    OSX_X86_32 = "osx-32"
     WIN_64 = "win-64"
     WIN_32 = "win-32"
     NO_ARCH = "noarch"
@@ -46,75 +55,75 @@ class Architecture(str, Enum):
 _BASE_REPODATA_URL: Final[str] = "https://repo.anaconda.com/pkgs/"
 
 # Maps the available architectures per channel hosted on `repo.anaconda.com`
-_SUPPORTED_CHANNEL_ARCH = Final[dict[Channel, set[Architecture]]] = {
+_SUPPORTED_CHANNEL_ARCH: Final[dict[Channel, set[Architecture]]] = {
     Channel.MAIN: {
-        Architecture.LINUX_x86_64,
-        Architecture.LINUX_x86_32,
+        Architecture.LINUX_X86_64,
+        Architecture.LINUX_X86_32,
         Architecture.LINUX_GRAVITON_2,
         Architecture.LINUX_S390,
         Architecture.LINUX_PPC64LE,
-        Architecture.OSX_x86_64,
+        Architecture.OSX_X86_64,
         Architecture.OSX_ARM64,
         Architecture.WIN_64,
         Architecture.WIN_32,
         Architecture.NO_ARCH,
     },
     Channel.FREE: {
-        Architecture.LINUX_x86_64,
-        Architecture.LINUX_x86_32,
-        Architecture.LINUX_ARM_v6l,
-        Architecture.LINUX_ARM_v7l,
+        Architecture.LINUX_X86_64,
+        Architecture.LINUX_X86_32,
+        Architecture.LINUX_ARM_V6L,
+        Architecture.LINUX_ARM_V7L,
         Architecture.LINUX_PPC64LE,
-        Architecture.OSX_x86_64,
-        Architecture.OSX_x86_32,
+        Architecture.OSX_X86_64,
+        Architecture.OSX_X86_32,
         Architecture.WIN_64,
         Architecture.WIN_32,
         Architecture.NO_ARCH,
     },
     Channel.R: {
-        Architecture.LINUX_x86_64,
-        Architecture.LINUX_x86_32,
-        Architecture.LINUX_ARM_v6l,
-        Architecture.LINUX_ARM_v7l,
+        Architecture.LINUX_X86_64,
+        Architecture.LINUX_X86_32,
+        Architecture.LINUX_ARM_V6L,
+        Architecture.LINUX_ARM_V7L,
         Architecture.LINUX_PPC64LE,
-        Architecture.OSX_x86_64,
-        Architecture.OSX_x86_32,
+        Architecture.OSX_X86_64,
+        Architecture.OSX_X86_32,
         Architecture.WIN_64,
         Architecture.WIN_32,
         Architecture.NO_ARCH,
     },
     Channel.PRO: {
-        Architecture.LINUX_x86_64,
-        Architecture.LINUX_x86_32,
-        Architecture.LINUX_ARM_v6l,
-        Architecture.LINUX_ARM_v7l,
+        Architecture.LINUX_X86_64,
+        Architecture.LINUX_X86_32,
+        Architecture.LINUX_ARM_V6L,
+        Architecture.LINUX_ARM_V7L,
         Architecture.LINUX_PPC64LE,
-        Architecture.OSX_x86_64,
-        Architecture.OSX_x86_32,
+        Architecture.OSX_X86_64,
+        Architecture.OSX_X86_32,
         Architecture.WIN_64,
         Architecture.WIN_32,
         Architecture.NO_ARCH,
     },
     Channel.ARCHIVE: {
-        Architecture.LINUX_x86_64,
-        Architecture.LINUX_x86_32,
-        Architecture.LINUX_ARM_v6l,
-        Architecture.LINUX_ARM_v7l,
+        Architecture.LINUX_X86_64,
+        Architecture.LINUX_X86_32,
+        Architecture.LINUX_ARM_V6L,
+        Architecture.LINUX_ARM_V7L,
         Architecture.LINUX_PPC64LE,
-        Architecture.OSX_x86_64,
-        Architecture.OSX_x86_32,
+        Architecture.OSX_X86_64,
+        Architecture.OSX_X86_32,
         Architecture.WIN_64,
         Architecture.WIN_32,
         Architecture.NO_ARCH,
     },
     Channel.MRO_ARCHIVE: {
-        Architecture.LINUX_x86_64,
-        Architecture.LINUX_x86_32,
-        Architecture.LINUX_ARM_v6l,
-        Architecture.LINUX_ARM_v7l,
+        Architecture.LINUX_X86_64,
+        Architecture.LINUX_X86_32,
+        Architecture.LINUX_ARM_V6L,
+        Architecture.LINUX_ARM_V7L,
         Architecture.LINUX_PPC64LE,
-        Architecture.OSX_x86_64,
-        Architecture.OSX_x86_32,
+        Architecture.OSX_X86_64,
+        Architecture.OSX_X86_32,
         Architecture.WIN_64,
         Architecture.WIN_32,
         Architecture.NO_ARCH,
@@ -131,16 +140,15 @@ class ApiException(BaseApiException):
     Indicates an exception occurred with this API.
     """
 
-    def __init__(self, message: str):
-        """
-        Constructs an API exception
-        :param message: String description of the issue encountered.
-        """
-        super().__init__(message)
+    pass
 
 
 @dataclass
 class RepodataMetadata:
+    """
+    Metadata section in a `repodata.json` blob
+    """
+
     # Required fields
     subdir: str
     # Optional fields
@@ -150,6 +158,10 @@ class RepodataMetadata:
 
 @dataclass
 class PackageData:
+    """
+    Per-package data stored in a `repodata.json` blob
+    """
+
     # Required fields
     build: str
     build_number: int
@@ -169,6 +181,10 @@ class PackageData:
 
 @dataclass
 class Repodata:
+    """
+    Structure that contains an entire serialized `repodata.json` blob
+    """
+
     info: RepodataMetadata
     packages: dict[str, PackageData]
     removed: list[str]
@@ -188,7 +204,22 @@ _REPODATA_JSON_SCHEMA: Final[SchemaType] = {
             },
         },
         "packages": {
-            # TODO
+            "required": ["build", "build_number", "depends", "md5", "sha256", "name", "size", "timestamp", "version"],
+            "properties": {
+                "build": {"type": "string"},
+                "build_number": {"type": "integer"},
+                "depends": {"type": "array", "items": {"type": "string"}},
+                "md5": {"type": "string"},
+                "sha256": {"type": "string"},
+                "name": {"type": "string"},
+                "size": {"type": "integer"},
+                "timestamp": {"type": "string"},
+                "version": {"type": "string"},
+                "date": {"type": "string"},
+                "track_features": {"type": "string"},
+                "license": {"type": "string"},
+                "license_family": {"type": "string"},
+            },
         },
         "removed": {"type": "array", "items": {"type": "string"}},
         "repodata_version": {"type": "integer"},
